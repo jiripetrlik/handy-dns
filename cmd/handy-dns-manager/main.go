bb5f77a9094da3c84b065035c7732a1a88a1cd30
Jiri Petrlik <jiripetrlik@gmail.com>
2019-04-07 15:00:09 +0200

    REST API

--- cmd/handy-dns-manager/main.go
+++ cmd/handy-dns-manager/main.go
@@ -1,5 +1,94 @@
 package main
 
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type ZoneItem struct {
+	Id       int64
+	Name     string
+	Class    string
+	ItemType string
+	Data     string
+}
+
+func createTestItemsList() []ZoneItem {
+	item1 := ZoneItem{
+		1,
+		"text1",
+		"IN",
+		"NS",
+		"127.0.0.1"}
+	item2 := ZoneItem{
+		2,
+		"text2",
+		"IN",
+		"A",
+		"127.0.0.2"}
+	items := []ZoneItem{item1, item2}
+
+	return items
+}
+
+func endpointListItems(writer http.ResponseWriter, request *http.Request) {
+	itemsList := createTestItemsList()
+	itemsListJSON, _ := json.Marshal(itemsList)
+
+	writer.Header().Set("Content-Type", "text/json")
+	io.WriteString(writer, string(itemsListJSON))
+	log.Printf("List with %v zone items was send", len(itemsList))
+}
+
+func endpointCreateItem(writer http.ResponseWriter, request *http.Request) {
+	var item ZoneItem
+
+	item.Name = request.URL.Query().Get("name")
+	item.Class = request.URL.Query().Get("class")
+	item.ItemType = request.URL.Query().Get("itemType")
+	item.Data = request.URL.Query().Get("data")
+
+	id := 5
+	jsonID, _ := json.Marshal(id)
+	writer.Header().Set("Content-Type", "text/json")
+	io.WriteString(writer, string(jsonID))
+
+	log.Printf("Item %v was created with id %v", item, id)
+}
+
+func endpointUpdateItem(writer http.ResponseWriter, request *http.Request) {
+	var item ZoneItem
+
+	item.Id, _ = strconv.ParseInt(request.URL.Query().Get("id"), 10, 64)
+	item.Name = request.URL.Query().Get("name")
+	item.Class = request.URL.Query().Get("class")
+	item.ItemType = request.URL.Query().Get("itemType")
+	item.Data = request.URL.Query().Get("data")
+
+	jsonItem, _ := json.Marshal(item)
+	writer.Header().Set("Content-Type", "text/json")
+	io.WriteString(writer, string(jsonItem))
+
+	log.Printf("Item %v was updated", item)
+}
+
+func endpointDeleteItem(writer http.ResponseWriter, request *http.Request) {
+	id, _ := strconv.ParseInt(request.URL.Query().Get("id"), 10, 64)
+
+	jsonID, _ := json.Marshal(id)
+	writer.Header().Set("Content-Type", "text/json")
+	io.WriteString(writer, string(jsonID))
+
+	log.Printf("Item %v was deleted", id)
+}
+
 func main() {
-	println("Hello world!")
+	http.HandleFunc("/api/list", endpointListItems)
+	http.HandleFunc("/api/create", endpointCreateItem)
+	http.HandleFunc("/api/update", endpointUpdateItem)
+	http.HandleFunc("/api/delete", endpointDeleteItem)
+	http.ListenAndServe(":8080", nil)
 }
