6823b82f01e408e92ae2cc3fdafc2d6018dc9320
Jiri Petrlik <jiripetrlik@gmail.com>
2019-04-13 22:50:32 +0200

    Parsing of command line arguments

--- cmd/handy-dns-manager/main.go
+++ cmd/handy-dns-manager/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"flag"
+	"log"
 	"net/http"
 
 	"github.com/jiripetrlik/handy-dns/internal/app/rest"
 )
 
 func main() {
+	zoneNamePtr := flag.String("n", "example-domain", "Domain name")
+	zoneFilePtr := flag.String("f", "example-domain.hosts", "Zone file")
+	zoneDataPtr := flag.String("d", "example-domain.json", "Data about zone")
+	flag.Parse()
+
+	log.Printf(
+		"Starint handy-dns-manager for domain %v. Zonefile=%v and zone data=%v",
+		*zoneNamePtr,
+		*zoneFilePtr,
+		*zoneDataPtr,
+	)
+
 	rest.HandleRestAPI()
 	http.ListenAndServe(":8080", nil)
 }
