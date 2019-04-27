ae70c06cd37f7dfbc4e214260f35825419f0cc5c
Jiri Petrlik <jiripetrlik@gmail.com>
2019-04-27 21:59:42 +0200

    Error handling in rest package

--- internal/app/rest/rest.go
+++ internal/app/rest/rest.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/jiripetrlik/handy-dns/internal/app/dnszone"
 )
@@ -14,16 +15,40 @@ type HandyDnsRestServer struct {
 	DNSZone *dnszone.DNSZone
 }
 
-func (s *HandyDnsRestServer) endpointListItems(writer http.ResponseWriter, request *http.Request) {
+type appError struct {
+	Error   error
+	Message string
+	Code    int
+}
+
+type appHandler func(http.ResponseWriter, *http.Request) *appError
+
+func (fn appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := fn(w, r)
+	if err != nil {
+		log.Printf("Error processing http request. Caused by %v", err.Error)
+		http.Error(w, err.Message, err.Code)
+	}
+}
+
+func (s *HandyDnsRestServer) endpointListItems(writer http.ResponseWriter, request *http.Request) *appError {
 	itemsList := s.DNSZone.GetZoneData().ZoneItems
-	itemsListJSON, _ := json.Marshal(itemsList)
+	itemsListJSON, err := json.Marshal(itemsList)
+	if err != nil {
+		return &appError{
+			Error:   err,
+			Message: "Can not marshal zone items. Caused by " + err.Error(),
+			Code:    500,
+		}
+	}
 
 	writer.Header().Set("Content-Type", "text/json")
 	io.WriteString(writer, string(itemsListJSON))
 	log.Printf("List with %v zone items was send", len(itemsList))
+	return nil
 }
 
-func (s *HandyDnsRestServer) endpointCreateItem(writer http.ResponseWriter, request *http.Request) {
+func (s *HandyDnsRestServer) endpointCreateItem(writer http.ResponseWriter, request *http.Request) *appError {
 	item := dnszone.ZoneItem{
 		ID:       0,
 		Name:     request.URL.Query().Get("name"),
@@ -35,10 +60,18 @@ func (s *HandyDnsRestServer) endpointCreateItem(writer http.ResponseWriter, requ
 	s.DNSZone.AddZoneItem(item)
 
 	log.Printf("Item %v was created with id %v", item, item.ID)
+	return nil
 }
 
-func (s *HandyDnsRestServer) endpointUpdateItem(writer http.ResponseWriter, request *http.Request) {
-	id, _ := strconv.ParseInt(request.URL.Query().Get("id"), 10, 64)
+func (s *HandyDnsRestServer) endpointUpdateItem(writer http.ResponseWriter, request *http.Request) *appError {
+	id, err := strconv.ParseInt(request.URL.Query().Get("id"), 10, 64)
+	if err != nil {
+		return &appError{
+			Error:   err,
+			Message: "Unable to convert id to int64",
+			Code:    404,
+		}
+	}
 	item := dnszone.ZoneItem{
 		ID:       id,
 		Name:     request.URL.Query().Get("name"),
@@ -47,26 +80,70 @@ func (s *HandyDnsRestServer) endpointUpdateItem(writer http.ResponseWriter, requ
 		Data:     request.URL.Query().Get("data"),
 	}
 
-	s.DNSZone.UpdateZoneItem(item)
+	err = s.DNSZone.UpdateZoneItem(item)
+	if err != nil {
+		if strings.Contains(err.Error(), "does not exist") {
+			return &appError{
+				Error:   err,
+				Message: "Item does not exist",
+				Code:    404,
+			}
+		}
+		return &appError{
+			Error:   err,
+			Message: "Error updating item. Caused by " + err.Error(),
+			Code:    500,
+		}
+	}
 
-	jsonItem, _ := json.Marshal(item)
+	jsonItem, err := json.Marshal(item)
+	if err != nil {
+		return &appError{
+			Error:   err,
+			Message: "Can not marshal item. Caused by " + err.Error(),
+			Code:    500,
+		}
+	}
 	writer.Header().Set("Content-Type", "text/json")
 	io.WriteString(writer, string(jsonItem))
 
 	log.Printf("Item %v was updated", item)
+	return nil
 }
 
-func (s *HandyDnsRestServer) endpointDeleteItem(writer http.ResponseWriter, request *http.Request) {
-	id, _ := strconv.ParseInt(request.URL.Query().Get("id"), 10, 64)
+func (s *HandyDnsRestServer) endpointDeleteItem(writer http.ResponseWriter, request *http.Request) *appError {
+	id, err := strconv.ParseInt(request.URL.Query().Get("id"), 10, 64)
+	if err != nil {
+		return &appError{
+			Error:   err,
+			Message: "Unable to convert id to int64",
+			Code:    404,
+		}
+	}
 
-	s.DNSZone.DeleteZoneItem(id)
+	err = s.DNSZone.DeleteZoneItem(id)
+	if err != nil {
+		if strings.Contains(err.Error(), "does not exist") {
+			return &appError{
+				Error:   err,
+				Message: "Item does not exist",
+				Code:    404,
+			}
+		}
+		return &appError{
+			Error:   err,
+			Message: "Error deleting item. Caused by " + err.Error(),
+			Code:    500,
+		}
+	}
 
 	log.Printf("Item %v was deleted", id)
+	return nil
 }
 
 func (s *HandyDnsRestServer) HandleRestAPI() {
-	http.HandleFunc("/api/list", s.endpointListItems)
-	http.HandleFunc("/api/create", s.endpointCreateItem)
-	http.HandleFunc("/api/update", s.endpointUpdateItem)
-	http.HandleFunc("/api/delete", s.endpointDeleteItem)
+	http.Handle("/api/list", appHandler(s.endpointListItems))
+	http.Handle("/api/create", appHandler(s.endpointCreateItem))
+	http.Handle("/api/update", appHandler(s.endpointUpdateItem))
+	http.Handle("/api/delete", appHandler(s.endpointDeleteItem))
 }
